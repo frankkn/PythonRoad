e6ad1f9d6421ad4079959cf1a6b52b5faf1639dc
Frank <frank840629@gmail.com>
2023-03-05 18:36:38 +0800

    update

---
+++ PyMoney/Week4_Python_Applications_and_String/110062619_楊淨富_hw1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+deposit = (int)(input('How much money do you have? '))
+record = input('Add an expense or income record with description and amount:\n').split(' ')
+expense = (int)(record[1])
+print('Now you have %d dollars.' % (deposit + expense))
