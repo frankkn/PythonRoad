cdc814e8feb7f4ff0b0449f3752d5967a422a8e3
Frank <frank840629@gmail.com>
2023-03-15 01:45:00 +0800

    update

--- PyMoney/Week4_Python_Applications_and_String/pymoney.py
+++ PyMoney/Week4_Python_Applications_and_String/pymoney.py
@@ -3,4 +3,5 @@
 deposit = (int)(input('How much money do you have? '))
 record = input('Add an expense or income record with description and amount:\n').split(' ')
 expense = (int)(record[1])
-print('Now you have %d dollars.' % (deposit + expense))
+# print('Now you have %d dollars.' % (deposit + expense))
+print(f'Now you have {deposit + expense} dollars.')
--- Unit5_applications/show_arg.py
+++ Unit5_applications/show_arg.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+# import sys
+# print(sys.argv)
+# print(len(sys.argv))
+# print(sys.argv[1:])
+
 import sys
-print(sys.argv)
+sys.stderr.write('cannot open input file %s\n' % sys.argv[1])
