7a98e5de48268e75b15610e11baa6cb0b7ff2591
Frank <frank840629@gmail.com>
2023-04-04 02:38:41 +0800

    update pymoney week5&6

---
+++ PyMoney/Week5_Sequence_Set_Dictionary/pymoney.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+def transform_str_to_list(recordstr):
+  records = recordstr.split(', ')
+  record_list = []
+  for record in records:
+    name, value = record.split()
+    record_list.append((name, int(value))) # Required step 2
+  print(record_list)
+  return record_list
+
+def cal_expense(record_list):
+  # Given a record list, sum up all the expense
+  expense = 0
+  for _,value in record_list:
+    expense += value
+  return expense
+
+def output(recordstr, deposit):
+  '''
+  1. Print desired output format of records
+  2. Calculate expense from records 
+  3. Print the balance
+  '''
+  print(r"Here's your expense and income records:")
+  print('\n'.join(rec for rec in recordstr.split(', ')))
+  record_list = transform_str_to_list(recordstr)
+  expense = cal_expense(record_list)
+  print(f'Now you have {deposit + expense} dollars.')
+
+def cal_balance(recordstr, deposit):
+  record_list = transform_str_to_list(recordstr)
+  exp = cal_expense(record_list)
+  return deposit + exp
+
+deposit = (int)(input('How much money do you have? '))
+recordstr = input('Add some expense or income records with description and amount:\n')
+output(recordstr, deposit)
+
+if	__name__	==	'__main__':
+  assert	cal_balance(recordstr = "breakfast -50, lunch -70, dinner -100, salary 3500", deposit = 1000) \
+            ==	4280
+
+
+
---
+++ PyMoney/Week6_Control_Constructs/pymoney.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+D = {}
+deposit = 0
+
+def updateDeposit(*values):
+  global deposit
+  for v in values:
+    if isinstance(v, int):
+      deposit += v
+    else: 
+      for i in v:
+        deposit += i
+
+def start():
+  global D
+  global deposit 
+  deposit = (int)(input('How much money do you have? '))
+  print('')
+  while True:
+    cmd = input('What do you want to do (add / view / delete / exit)?: ').lower() # accept both VIEW or view
+    if cmd == 'exit':
+      break
+    elif cmd == 'add':
+      name, value = input('Add an expense or income record with description and amount:\n').split()
+      if name in D.keys():
+        D[name].append(int(value))
+      else:
+        D[name] = [int(value)]
+      updateDeposit(int(value))
+      print('')
+    elif cmd == 'view':
+      print("Here's your expense and income records:\n")
+      print("%-20s %s" % ("Desciption", "Amount")) # left-aligned
+      print("="*20 +  " " + "=" * 6)
+      for name, values in D.items():
+        print(f'{name:<20s}', end = " ")
+        print(f"{','.join(str(i) for i in values).lstrip('[').rstrip(']')}")
+      print("="*20 +  " " + "=" * 6)
+      print(f'Now you have {deposit} dollars.')
+      print('')
+    elif cmd == 'delete':
+      del_list = input('Which record do you want to delete?\n').split()
+      # Enter name + amount to delete one record or name to delete all of records relating to name  
+      if len(del_list) == 1:
+        list_sum = sum(D[''.join(del_list)])
+        updateDeposit(-1 * list_sum)
+        del D[''.join(del_list)] # tranform list into str
+      else:
+        name, value = del_list[0], del_list[1]
+        if int(value) in D[name]:
+          updateDeposit(-1 * int(value))
+          D[name].remove(int(value))
+          if len(D[name]) == 0:
+            del D[name]
+        else:
+          raise ValueError(f'invalid input: {value} not found')
+      print('')
+    else:
+      raise ValueError(f'invalid input: {cmd}')
+
+start()
+# if	__name__	==	'__main__':
+#   start()
+
+
+
